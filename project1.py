91e2238f81e601334236246ae2fbd97473aaff2d
ceaser444 <okaforemma888@gmail.com>
2025-11-20 22:57:26 +0100

    Add Project 1

---
+++ project1.py
@@ -0,0 +1,5 @@
+#frist coding 
+name = input("Your name: ")
+state = input("State of Origin: ").upper()
+gender = input("Male or Female: ")
+print(f"My name is {name}, i was born in {state}, i am a {gender}")
